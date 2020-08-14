df34d301317377a148f887f9a2b64472b4255489
Thavi <thavit97@gmail.com>
2020-08-14 12:19:44 +0100

    rotate and pan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,10 +12,20 @@
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
       <script>
       $(document).ready(function(){
-        $("button").click(function(){
-          $("#bob").animate({left: '1000px'});
+        $(".moveright").click(function(){
+          $(".moveright").animate({left: '1000px'});
         });
       });
+
+      function bigImg(x) {
+        x.style.height = "auto";
+        x.style.width = "700px";
+      }
+
+      function normalImg(x) {
+        x.style.height = "auto";
+        x.style.width = "500px";
+      }
       </script>
 
       <title>Tree Exercise</title>
@@ -32,40 +42,55 @@
         <!-- Links -->
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#section1">Block 1</a>
+            <a class="nav-link" href="#section1">Move Right</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#section2">Block 2</a>
+            <a class="nav-link" href="#section2">Hover</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#section3">Block 3</a>
+            <a class="nav-link" href="#section3">Rotate</a>
           </li>
         </ul>
       </nav>
 
       <div id="section1" class="container-fluid">
-        <h1>Block 1</h1>
-        <p>This block showcases an onclick event. Click the move image button below!</p>
+        <h1>Move Right</h1>
+        <p>This block showcases a jQuery onclick event. Click the image below to move it!</p>
         <div class="moveright">
            <img src="/img/ladybird.jpg" alt="ladybird">
         </div>
-        <div>
-          <button>Move Image</button>
-        </div>
       </div>
       <div id="section2" class="container-fluid">
-        <h1>Block 2</h1>
-        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <h1>Hover</h1>
+        <p>This block showcases two hover events. The image on the left is a CSS hover. The image on the right is a jQuery hover.</p>
         <div class="col-lg-12">
-          <div class="col-lg-6">
-            <img class="zoom" src="/img/jaguar.jpg" alt="jaguar">
+          <div class="row">
+            <div class="col-lg-6">
+              <img class="zoomcss" src="/img/jaguar.jpg" alt="jaguar">
+            </div>
+            <div class="col-lg-6">
+              <img class="zoomjquery" onmouseover="bigImg(this)" onmouseout="normalImg(this)" src="/img/path.jpg" alt="path">
+            </div>
           </div>
         </div>
       </div>
       <div id="section3" class="container-fluid">
-        <h1>Block 3</h1>
-        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
-        <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation bar while scrolling!</p>
+        <h1>Rotate</h1>
+        <p>This block showcases CSS3 animations. The first image is constantly rotating. The second image is constantly panning.</p>
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-6">
+              <img class="rotate" src="/img/station.jpg" alt="">
+            </div>
+            <div class="col-lg-6">
+              <img class="pan" src="/img/station.jpg" alt"">
+            </div>
+          </div>
+        </div>
       </div>
+      <div id="section4" class="container-fluid">
+        <h1>Contact Us</h1>
+        <p>This block showcases CSS3 animations. The first image is constantly rotating. The second image is constantly panning.</p>
+
     </body>
 </html>
